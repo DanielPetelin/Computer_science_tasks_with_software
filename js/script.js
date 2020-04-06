5506feca9e6d12d7588ed0c44cb0c091f288c036
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-06 14:58:26 +0300

    fefact fun

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 let arr = [];
+let out = '';
+let yourNum = document.querySelector('#num').value;
 
 function sp() {
-    let result = document.querySelector('#num').value /= 2;
+    let result = yourNum /= 2;
+    document.querySelector('#num').value /= 2;
 
     arr.push(1);
     document.querySelector('#out').innerHTML = result + ' ваш выбор ( 1 )';
+
+    if (arr.length == 5 && yourNum == 3) {
+        document.querySelector('#out2').innerHTML = 'Это гениально!';
+    };
 };
 
 function delOne() {
-    let result = document.querySelector('#num').value -= 1;
+    let result = yourNum -= 1;
+    document.querySelector('#num').value--;
     
     arr.push(2);
     document.querySelector('#out').innerHTML = result + ' ваш выбор ( 1 )';
+
+    if (arr.length == 5 && yourNum == 3) {
+        document.querySelector('#out2').innerHTML = 'Это гениально!';
+    };
 };
 
 console.log(arr);
