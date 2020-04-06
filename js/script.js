b557dc435d1cc2f2ea5f7e68aa7b37cb5439938b
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-06 14:39:37 +0300

    add new funs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-function sp() {
-    let num = +document.querySelector('num').value;
-    let result = num / 2;
+let arr = [];
 
-    document.querySelector('#out').innerHTML = result;
+function sp() {
+    let result = document.querySelector('#num').value /= 2;
+
+    arr.push(1);
+    document.querySelector('#out').innerHTML = result + ' ваш выбор ( 1 )';
 };
+
+function delOne() {
+    let result = document.querySelector('#num').value -= 1;
+    
+    arr.push(2);
+    document.querySelector('#out').innerHTML = result + ' ваш выбор ( 1 )';
+};
+
+console.log(arr);
